984afc813ab5c8912b7e4acab81f264a68773291
Stefaan Ponnet <stefaan@ponnet.com>
2015-08-06 23:14:15 +0200

    538 already provides youtube and spotify IDs - use them..

--- fetchers/538.js
+++ fetchers/538.js
@@ -63,6 +63,8 @@ module.exports = {
 						thumb: track.image,
 						title: track.title,
 						unixtimestamp: track.time_gmt,
+						spotifyurl : track.spotify_url,
+						youtubeid : track.youtube_id
 					};
 					console.log('song to be saved : ', songdata);
 					playlistdata.postSong(songdata);
